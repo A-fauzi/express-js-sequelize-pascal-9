c3e1df4cc6e2d9ccdbbd458677218d837d0bf25a
Akhmad Fauzi <akhmadfauzi9797@gmail.com>
2022-08-18 01:13:48 +0700

    add hash many flow clean arch

--- src/config/db.migration.js
+++ src/config/db.migration.js
@@ -1,16 +1,21 @@
 // Todo -> 1. db migration membutuhkan customer model untuk meregister pembuatan table model customer
 const Customer = require('../model/customer.model')
 const User = require('../model/user.model')
+const Address = require('../model/address.model');
+const Product = require('../model/product.model');
+
 
 // Todo -> 2. buat function untuk meregister sebuah table model dengan method sync()
 //  dan membutuhkan argument parameter db untuk kebutuhan Model Customer
 module.exports = async (db) => {
     const customer = await Customer(db)
     const user = await User(db)
+    const address = Address(db);
+    const product = Product(db);
 
-    customer.hasOne(user)
-    user.belongsTo(customer)
-
-    await customer.sync()
-    await user.sync()
+    user.belongsTo(customer);
+    customer.hasMany(address);
+    customer.belongsToMany(product, { through: 'r_product_customer' });
+    product.belongsToMany(customer, { through: 'r_product_customer' });
+    await db.sync()
 }
